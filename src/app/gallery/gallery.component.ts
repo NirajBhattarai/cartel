b57ef63c34ac53cd3b59706dafb4ffdb78d47806
Rahul Yadav <rahul54784@gmail.com>
2022-03-15 19:27:58 +0530

    Revert "Gallery page infinity scroll added"
    This reverts commit 72ac8090b2ee22585df339e99b386ce029ad631d.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Lightbox } from 'ngx-lightbox';
 
-
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -9,27 +8,11 @@ import { Lightbox } from 'ngx-lightbox';
 })
 export class GalleryComponent implements OnInit {
   public imageArray: any = [];
-  public startIndex = 1;
-  public lastIndex = 100;
   // public imageArray = Array.from(Array(1000).keys());
   constructor(
     public _lightbox: Lightbox
   ) {
-    this.fetchImages();
-  }
-
-  ngOnInit(): void {}
-
-  open(index: number): void {
-    this._lightbox.open(this.imageArray, index);
-  }
-
-  close(): void {
-    this._lightbox.close();
-  }
-
-  fetchImages(){
-    for (let i = this.startIndex; i <= this.lastIndex; i++) {
+    for (let i = 1; i <= 1000; i++) {
       const src = 'assets/img/gallery/' + i + '.png';
       const caption = 'Full Image';
       const thumb = 'assets/img/gallery/' + i + '.png';
@@ -43,13 +26,14 @@ export class GalleryComponent implements OnInit {
     }
   }
 
-  onScroll(){
-    if(this.lastIndex < 1000){
-      this.startIndex = this.lastIndex + 1;
-      this.lastIndex = this.lastIndex + 100;
-      this.fetchImages();  
-    }
-    
+  ngOnInit(): void {}
+
+  open(index: number): void {
+    this._lightbox.open(this.imageArray, index);
+  }
+
+  close(): void {
+    this._lightbox.close();
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,13 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { GalleryComponent } from '../gallery/gallery.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 const COMPONENTS = [HomeComponent,GalleryComponent];
 const MODULES = [CommonModule, ComponentsModule]
 
 @NgModule({
   declarations: [...COMPONENTS, PageNotFoundComponent],
-  imports: [...MODULES,InfiniteScrollModule]
+  imports: [...MODULES]
 })
 export class PagesModule { }
